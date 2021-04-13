279a18c9cc0c678df838962949f9d8889baaabcd
rgmishan <mishanregmi@gmail.com>
2021-04-14 03:09:45 +0530

    database edit

--- admin_area/includes/db.php
+++ admin_area/includes/db.php
@@ -1,3 +1,4 @@
 <?php
-$con = mysqli_connect("localhost", "root", "", "wasdroot");
+$con = mysqli_connect("gstanzadb2.cxqu4aok5wdg.ap-south-1.rds.amazonaws.com", "admin", "adminredhat123", "wasdroot") or die("Database Connection Unsuccessful ! .");
+
 ?>
--- customer/includes/db.php
+++ customer/includes/db.php
@@ -1,4 +1,4 @@
 <?php
-$con = mysqli_connect("localhost", "root", "", "wasdroot") or die("Database Connection Unsuccessful ! .");
+$con = mysqli_connect("gstanzadb2.cxqu4aok5wdg.ap-south-1.rds.amazonaws.com", "admin", "adminredhat123", "wasdroot") or die("Database Connection Unsuccessful ! .");
 
 ?>
