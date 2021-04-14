1e931840a97a609fb4beceab31b393c623ab93fb
rgmishan <mishanregmi@gmail.com>
2021-04-14 17:19:24 +0530

    localhost changed to rds

--- customer/functions/functions.php
+++ customer/functions/functions.php
@@ -1,5 +1,6 @@
 <?php
-$db= mysqli_connect("localhost", "root", "", "wasdroot");
+$db= mysqli_connect("gstanzadb2.cxqu4aok5wdg.ap-south-1.rds.amazonaws.com", "admin", "adminredhat123", "wasdroot") or die("Database Connection Unsuccessful ! .");
+
 function getPro(){
 	global $db;
 	$get_product="SELECT * FROM products order by 1 DESC LIMIT 0,6";
--- functions/functions.php
+++ functions/functions.php
@@ -1,5 +1,6 @@
 <?php
-$db = mysqli_connect("localhost", "root", "", "wasdroot");
+$db = mysqli_connect("gstanzadb2.cxqu4aok5wdg.ap-south-1.rds.amazonaws.com", "admin", "adminredhat123", "wasdroot") or die("Database Connection Unsuccessful ! .");
+
 
 
 //FOR GETTING USER IP ADDRESS STARTED
