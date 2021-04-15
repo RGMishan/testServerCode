27390bb34deb742712e09c5b0057792bba464cc2
rgmishan <mishanregmi@gmail.com>
2021-04-15 23:44:49 +0530

    Env Variable added

--- customer/functions/functions.php
+++ customer/functions/functions.php
@@ -1,5 +1,5 @@
 <?php
-$db= mysqli_connect("gstanzadb2.cxqu4aok5wdg.ap-south-1.rds.amazonaws.com", "admin", "adminredhat123", "wasdroot") or die("Database Connection Unsuccessful ! .");
+$db = mysqli_connect(getenv("GS_HOST"), getenv("GS_USER"), getenv("GS_PASS"), getenv("GS_DB")) or die("Database Connection Unsuccessful ! .");
 
 function getPro(){
 	global $db;
--- customer/includes/db.php
+++ customer/includes/db.php
@@ -1,4 +1,3 @@
 <?php
-$con = mysqli_connect("gstanzadb2.cxqu4aok5wdg.ap-south-1.rds.amazonaws.com", "admin", "adminredhat123", "wasdroot") or die("Database Connection Unsuccessful ! .");
-
+$con = mysqli_connect(getenv("GS_HOST"), getenv("GS_USER"), getenv("GS_PASS"), getenv("GS_DB")) or die("Database Connection Unsuccessful ! .");
 ?>
